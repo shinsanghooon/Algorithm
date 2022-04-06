7acec70c821566515a0c4fea56e0e3c1164a0dec
shinsanghooon <shinsanghooon@gmail.com>
2022-04-07 01:24:19 +0900

    greedy 2

---
+++ greedy/greedy2.py
@@ -0,0 +1,17 @@
+n = map(int, input().split())
+data = list(map(int, input().split()))
+# 2, 3, 1, 2, 2
+
+data.sort()
+# 1, 2, 2, 2, 3
+
+
+count = 0
+now_count = 0
+for i in data:
+  now_count += 1
+  if now_count >= i:
+    count +=1 
+    now_count = 0
+
+print(count)
