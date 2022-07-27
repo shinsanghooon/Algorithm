246074171e2622363fec57e7e439afabb1a9d8c6
shinsanghooon <shinsanghooon@gmail.com>
2022-07-27 23:53:20 +0900

    stack&queue 알고리즘

--- java/src/algorithm/stackqueue/Emergency.java
+++ java/src/algorithm/stackqueue/Emergency.java
@@ -1,4 +1,38 @@
 package algorithm.stackqueue;
 
+import java.util.Scanner;
+
 public class Emergency {
+
+    private int solution(int n, int m, int[] arr) {
+        int answer = 1;
+
+        int target = arr[m];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= target) {
+                if (arr[i] == target) {
+                    if (i < m) {
+                        answer ++;
+                    }
+                } else {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Emergency T = new Emergency();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println(T.solution(n, m, arr));
+    }
 }
