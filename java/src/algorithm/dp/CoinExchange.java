8b16daa548445891e24e5b83b0e2a8727f773345
shinsanghooon <shinsanghooon@gmail.com>
2022-09-01 22:54:06 +0900

    동전 교환 문제

---
+++ java/src/algorithm/dp/CoinExchange.java
@@ -0,0 +1,43 @@
+package algorithm.dp;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CoinExchange {
+
+    static int n, m;
+    static int[] dy;
+
+    public int solution(int[] coin) {
+        Arrays.fill(dy, Integer.MAX_VALUE);
+
+        dy[0] = 0 ;
+        for (int i = 0; i < n; i++) {
+            for (int j = coin[i]; j <= m; j++) {
+                dy[j] = Math.min(dy[j], dy[j - coin[i]] + 1);
+            }
+        }
+
+        return dy[m];
+    }
+
+
+    public static void main(String[] args) {
+        CoinExchange T = new CoinExchange();
+
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        m = sc.nextInt();
+
+        dy = new int[m + 1];
+        System.out.println(T.solution(arr));
+    }
+
+}
