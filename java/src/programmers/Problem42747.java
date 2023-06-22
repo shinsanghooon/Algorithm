f19f2f2ce3faa928d2122756180e5d118164e297
shinsanghooon <shinsanghooon@gmail.com>
2023-06-22 09:11:02 +0900

    알고리즘 문제 풀이

---
+++ java/src/programmers/Problem42747.java
@@ -0,0 +1,39 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Problem42747 {
+
+
+    public int solution(int[] arr) {
+
+        int answer = 0;
+
+        Arrays.sort(arr);
+
+        int maxCitations = arr[arr.length - 1];
+
+        for (int h = maxCitations; h > 0; h--) {
+            int criteria = h;
+            long countOverH = Arrays.stream(arr).filter(e -> e >= criteria)
+                .count();
+
+            if (countOverH < criteria) {
+                continue;
+            }
+
+            answer = Math.max(answer, criteria);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Problem42747 s = new Problem42747();
+
+        int[] arr = new int[]{3, 0, 6, 1, 5};
+
+        int result = s.solution(arr);
+        System.out.println("result = " + result);
+    }
+
+}
